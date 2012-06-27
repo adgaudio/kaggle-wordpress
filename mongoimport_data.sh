35f69f2e66c3e8b58c3d708abbf92fc5a2899b20
Alex Gaudio <alex@adaptly.com>
2012-06-26 21:57:31 -0400

    update mongoimport with trainposts.json

--- mongoimport_data.sh
+++ mongoimport_data.sh
@@ -2,12 +2,9 @@ dbname="kaggle-wp"
 
 mongoimport -d $dbname -c tu --type json --file data/trainUsers.json
 mongoimport -d $dbname -c tpt --type json --file data/trainPostsThin.json
-
-#mongoimport -d $dbname -c tp --type json --file data/trainPosts.json
+mongoimport -d $dbname -c tp --type json --file data/trainPosts.json
 
 
 # Create indexes and preliminary data munging
-#echo "create count of likes in tpt..." | mongo $dbname
 echo "db.tpt.ensureIndex({'likes.uid': 1});" | mongo $dbname
 echo "db.tpt.ensureIndex({post_id: 1});" | mongo $dbname
-

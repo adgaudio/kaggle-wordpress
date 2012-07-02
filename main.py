f479a9232de7da50ccd8f1bebd9433c4b860f21b
Alex Gaudio <alex@adaptly.com>
2012-07-02 13:56:30 -0400

    make this work in virtualenv
    update main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import monary
 from numpy import matrix
 from pandas import DataFrame
+from pymongo import Connection
 
-conn = monary.Monary()
+DBNAME = 'kaggle-wp'
 
-dbname = 'kaggle-wp'
+def get_monary(conn=None, dbname=DBNAME, _cache={}):
+    if dbname not in _cache:
+        _cache[dbname] = conn or monary.Monary()
+    return [_cache[dbname], dbname]
 
-columns = ['blog', 'inTestSet', 'post_id', 'uid', 'like_dt']
-arrs = conn.query(dbname, 'tu2', {}, columns, ['int32']*4+['date'])
-df = matrix(arrs).transpose()
-df = DataFrame(df, columns = columns)
+def get_pymongo(dbname=DBNAME, _cache={}, *args, **kwargs):
+    if dbname not in _cache:
+        _cache[dbname] = Connection()[dbname]
+    return _cache[dbname]
+
+def get_tu():
+    conn, dbname = get_monary()
+    columns = ['blog', 'inTestSet', 'post_id', 'uid', 'like_dt']
+    arrs = conn.query(dbname, 'tu2', {}, columns, ['int32']*4+['date'])
+    df = matrix(arrs).transpose()
+    return DataFrame(df, columns = columns)
+
+def get_tp(conn=None):
+    conn, dbname = get_monary()
+    print "not implemented, but here's the tp collection using pymongo driver"
+    return get_pymongo.tp
+    #columns = []
+    #arrs = conn.query(dbname, '
 
 

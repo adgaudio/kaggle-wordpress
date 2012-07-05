b001716a0a66d7693b060e21f06c2eae02efe69a
Alex Gaudio <alex@adaptly.com>
2012-07-05 02:53:17 -0400

    refactor... found bug in tp2

--- clean_data.py
+++ clean_data.py
@@ -4,42 +4,18 @@ import datetime
 conn = Connection()
 db = conn['kaggle-wp']
 
-
-def _unwind_likes_from_collection(old_coll, db, new_coll):
-    new_coll.drop()
-    cur = old_coll.find()
-    #cur.batch_size(5000)
-    new_data = []
-    for n, elem in enumerate(cur):
-        elem.pop('_id')
-        likes = elem.pop('likes')
-        for m, like in enumerate(likes):
-            like.update(elem)
-            like = cast_vals_to_int(like)
-            new_data.append(like)
-        if (n+m) % 5000 == 0:
-            print 'batch inserting'
-            try:
-                new_coll.insert(new_data)
-            except:
-                print 'failed batch insert'
-                for x in new_data:
-                    new_coll.insert(x)
-            new_data = []
-
-def create_tu2(db):
-    _unwind_likes_from_collection(db.tu, db, db.tu2)
-
-def create_tpt2(db):
-    _unwind_likes_from_collection(db.tpt, db, db.tpt2)
-
-def create_tp2(db):
-    _unwind_likes_from_collection(db.tp, db, db.tp2)
+def unwind_likes_from(dct):
+    dct.pop('_id')
+    likes = dct.pop('likes')
+    for like in likes:
+        like.update(dct)
+        like = cast_vals_to_int(like)
+        yield like
 
 def cast_vals_to_int(dct):
     for k in dct:
         if k not in ['content', '_id', 'language', 'title', 
-                'url', 'tags', 'blogname', 'categories']:
+                'url', 'tags', 'blogname', 'categories', 'likes']:
             try:
                 dct[k] = int(dct[k])
             except:
@@ -47,8 +23,35 @@ def cast_vals_to_int(dct):
                     dct[k] = datetime.datetime.strptime(dct[k], '%Y-%m-%d %H:%M:%S')
                 except:
                     pass
-    #print dct
-    return dct
+    yield dct
+
+def data_munge(old_coll, db, new_coll, strategy=unwind_likes_from):
+    new_coll.drop()
+    cur = old_coll.find()
+    #cur.batch_size(5000)
+    new_data = []
+    for n, elem in enumerate(cur):
+        new_data.extend([x for x in strategy(elem)])
+
+        if (n) % 5000 == 0:
+            print len(new_data)
+            print 'batch inserting'
+            try:
+                new_coll.insert(new_data)
+            except:
+                print 'failed batch insert... inserting individually'
+                for x in new_data:
+                    new_coll.insert(x)
+            new_data = []
+
+def create_tu2(db):
+    data_munge(db.tu, db, db.tu2)
+
+def create_tpt2(db):
+    data_munge(db.tpt, db, db.tpt2)
+
+def create_tp2(db):
+    data_munge(db.tp, db, db.tp2, strategy=cast_vals_to_int)
 
 if __name__ == '__main__':
     print 'creating collection tu2'
